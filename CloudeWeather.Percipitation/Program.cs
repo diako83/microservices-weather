75faf90f4f49dee7a343bbbe6969e363a6ace9fb
diako83 <ismaildiako@gmail.com>
2023-09-03 18:12:08 +0200

    Add initial daps

--- CloudeWeather.Percipitation/Program.cs
+++ CloudeWeather.Percipitation/Program.cs
@@ -2,4 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+{
+    return Results.Ok(zip);
+});
+
+
+
 app.Run();
